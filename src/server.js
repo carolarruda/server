9ebab681281e4bb29f8bd15a25bffc7af5aa476f
carolarruda <carolinacalouroarruda@gmail.com>
2023-08-09 20:29:10 +0000

    added erd

--- src/server.js
+++ src/server.js
@@ -9,13 +9,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/recipes", recipeRouter);
+// app.use("/recipes", recipeRouter);
 
 app.get("*", (req, res) => {
   res.status(404).json({
     status: "fail",
     data: {
-      resource: "Not found",
+      resource: "Sorry, your resource was not found",
     },
   });
 });

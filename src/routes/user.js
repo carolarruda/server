fa3b2fdabf52d6477c60bbd6cc7b441648598b6a
carolina arruda <carolinacalouroarruda@gmail.com>
2023-12-05 19:28:54 +0100

    testing serviceAccount at root

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,10 @@ import { Router } from "express";
 import User from "../domain/user.js";
 import multer from "multer";
 import admin from 'firebase-admin'
-import serviceAccount  from "../serviceKey.json" assert { type: 'json' }; ;
+// import serviceAccount  from "../serviceKey.json" assert { type: 'json' } ;
+import serviceAccount  from "../serviceKey.json" assert { type: 'json' } ;
+import { PRIVATE_KEY } from '../utils/config.js'
+
 import {
   getAll,
   create,
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,5 @@
 export const JWT_SECRET = process.env.JWT_SECRET
 
 export const JWT_EXPIRY = process.env.JWT_EXPIRY
+
+export const PRIVATE_KEY = process.env.PRIVATE_KEY
